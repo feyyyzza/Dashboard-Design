270d92d7cb56ec1e5769ed4d036e31db7318fe27
Feyza Özsaray <116104065+feyyyzza@users.noreply.github.com>
2023-02-27 16:06:43 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+
+// kenar çubuğu //
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+}) 
