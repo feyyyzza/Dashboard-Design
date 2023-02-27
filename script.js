949a15ddd7e6fafec68317ac4863bc5f45e76e75
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-02-27 16:07:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
 
 // kenar çubuğu //
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 }) 
+
+
+// tema değiştirme //
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
